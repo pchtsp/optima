18f7054ecf7408d60256916f19a3d18d0246c8af
pchtsp <franco.peschiera@gmail.com>
2018-08-28 19:10:07 +0200

    changed R params formatting

--- R/functions/params.R
+++ R/functions/params.R
@@ -1,13 +1,15 @@
 library(magrittr)
 
 path_root = '/home/pchtsp/Documents/projects/'
+path_project = path_root %>% paste0("OPTIMA/") 
 path_results = '/home/pchtsp/Dropbox/OPTIMA_results/'
 
 PATHS = list(
     root= path_root
     ,results = path_results
     ,experiments = path_results %>% paste0("experiments/")
-    ,img = path_root %>% paste0("OPTIMA/img/")
-    ,latex = path_root %>% paste0("OPTIMA/latex/")
-    ,r_project = path_root %>% paste0("OPTIMA/R/")
+    ,img = path_root %>% paste0("img/")
+    ,latex = path_project %>% paste0("latex/")
+    ,r_project = path_project %>% paste0("R/")
+    ,data = path_project %>% paste0("data/")
 )
