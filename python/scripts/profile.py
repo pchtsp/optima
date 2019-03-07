eb2251012a39cf14832130c61a418c9e2298951e
Franco Peschiera <pchtsp@gmail.com>
2019-03-07 08:13:04 +0100

    added example to profile.py

--- python/scripts/profile.py
+++ python/scripts/profile.py
@@ -7,9 +7,10 @@ p.sort_stats('tottime').print_stats(20)  # tottime, cumulative
 # check_swap_size
 # check_sequence
 # try_change_node
-# python -m cProfile -o scripts/restats scripts/exec.py -pr C:/Users/Documents/projects/ROADEF2018/ -rr C:/Users/pchtsp/Dropbox/ROADEF2018/
+# python -m cProfile -o scripts/restats scripts/exec.py -p "{\"experiments\": \"C:/Users/pchtsp/Documents/borrar/experiments/\", \"results\": \"C:/Users/pchtsp/Documents/borrar/\"}"
 # python -m cProfile -o scripts/restats scripts/exec.py
 
 # for line profiling:
 # kernprof -l scripts/exec.py
+# kernprof -l scripts/exec.py -p "{\"experiments\": \"C:/Users/pchtsp/Documents/borrar/experiments/\", \"results\": \"C:/Users/pchtsp/Documents/borrar/\"}"
 # python -m line_profiler exec.py.lprof
