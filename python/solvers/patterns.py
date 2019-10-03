06832daa0f23508b5f6d8c5b43dd4a7e23b859e4
pchtsp <franco.peschiera@gmail.com>
2019-10-03 13:31:31 +0200

    added new way to generate patterns.

--- python/solvers/patterns.py
+++ python/solvers/patterns.py
@@ -3,8 +3,12 @@ import pytups.superdict as sd
 import ujson as json
 
 
-class Node(object):
+# installing graph-tool and adding it to venv:
+# https://git.skewed.de/count0/graph-tool/wikis/installation-instructions
+# https://jolo.xyz/blog/2018/12/07/installing-graph-tool-with-virtualenv
 
+
+class Node(object):
     """
     corresponds to a combination of: a period t, a maintenance m, and the remaining rets and ruts for all maintenances
     """
@@ -66,7 +70,7 @@ class Node(object):
         max_period = None
         # duration_past_maint = data_m.get_m(self.maint, 'duration_periods')
         # if duration_past_maint is None:
-            # duration_past_maint = 0
+        # duration_past_maint = 0
         for num, period in enumerate(self.iter_until_period(last)):
             acc_cons += self.get_consume(period)
             for m in maints_rut:
@@ -92,9 +96,9 @@ class Node(object):
 
         intersect = opts_rut.keys() & opts_ret.keys()
         int_dict = \
-            opts_ret.\
-                filter(intersect).\
-                apply(lambda k, v: opts_rut[k] & v).\
+            opts_ret. \
+                filter(intersect). \
+                apply(lambda k, v: opts_rut[k] & v). \
                 apply(lambda k, v: v if len(v) else opts_ret[k])
 
         opts_tot = opts_rut.clean(func=lambda v: v)
@@ -110,8 +114,8 @@ class Node(object):
     def calculate_rut(self, maint, period):
         """
         If next m=maint and is done in t=period, how does rut[m][t] change?
-        :param maint:
-        :param period:
+        :param str maint:
+        :param str period:
         :return:
         """
         maints_ruts = self.get_maints_ruts()
@@ -128,7 +132,7 @@ class Node(object):
         # if duration_past_maint is None:
         #     duration_past_maint = 0
         # there is an assumption that the next maintenance
-            # will always be duration_past_maint periods away.
+        # will always be duration_past_maint periods away.
 
         # for not affected maintenances, we reduce:
         # check if past maintenance had a duration.
@@ -152,8 +156,8 @@ class Node(object):
     def calculate_ret(self, maint, period):
         """
         If next m=maint and is done in t=period, how does ret change?
-        :param maint:
-        :param period:
+        :param str maint:
+        :param str period:
         :return:
         """
         # for each maint, except maint, I reduce the ret
@@ -203,7 +207,7 @@ class Node(object):
             maint = None
         ret = self.calculate_ret(maint, period)
         rut = self.calculate_rut(maint, period)
-        defaults = dict(instance = self.instance, resource=self.resource)
+        defaults = dict(instance=self.instance, resource=self.resource)
         return Node(period=period, maint=maint, rut=rut, ret=ret, **defaults)
 
     def get_data(self):
@@ -227,26 +231,36 @@ def walk_over_nodes(node, get_nodes_only=False):
     i = 0
     final_paths = []
     # this code gets all combinations
+    last_node = get_sink_node(node.instance, node.resource)
     while len(remaining_nodes) and i < 10000000:
         i += 1
         node, path = remaining_nodes.pop()
         # we need to make a copy of the path
         path = path + [node]
+        if node == last_node:
+            # if last_node reached, save the path and go back
+            final_paths.append(path)
+            continue
+        # we're not in the last_node.
         neighbors = cache_neighbors.get(node)
         if neighbors is None:
+            # I don't have any cache of the node.
+            # I'll get neighbors and do cache
             cache_neighbors[node] = neighbors = node.get_adjacency_list()
+            if not len(neighbors):
+                # no neighbors means I need to go to the last_node
+                cache_neighbors[node] = neighbors = [last_node]
         elif get_nodes_only:
+            # we have the cache AND we don't want paths, we leave
             continue
-        if not len(neighbors):
-            final_paths.append(path)
         remaining_nodes += [(n, path) for n in neighbors]
         print("iteration: {}, remaining: {}, stored: {}".format(i, len(remaining_nodes), len(cache_neighbors)))
-    if cache_neighbors:
+    if get_nodes_only:
         return cache_neighbors
     return final_paths
 
 
-def resource_to_node(instance, resource):
+def get_source_node(instance, resource):
     instance.data = sd.SuperDict.from_dict(instance.data)
     start = instance.get_param('start')
     period = instance.get_prev_period(start)
@@ -258,34 +272,122 @@ def resource_to_node(instance, resource):
     return Node(instance, resource, period, ret, rut, None)
 
 
-# def adjacency_to_graph(adjacency):
+def get_sink_node(instance, resource):
+    last = instance.get_param('end')
+    last_next = instance.get_next_period(last)
+    defaults = dict(instance=instance, resource=resource)
+    return Node(period=last_next, maint=None, rut=sd.SuperDict(), ret=sd.SuperDict(), **defaults)
 
 
+def get_create_node(refs, g, n):
+    v = refs.get(n)
+    if v is None:
+        v = g.add_vertex()
+        g.vp.period[v] = n.period
+        g.vp.maint[v] = n.maint
+        refs[n] = v
+    return v
+
+
+def adjacency_to_graph(nodes_ady):
+    try:
+        import graph_tool.all as gr
+    except:
+        print('graph-tool is not available')
+        return None
+
+    g = gr.Graph()
+    g.vp.period = g.new_vp('string')
+    g.vp.maint = g.new_vp('string')
+    g.ep.maint = g.new_ep('string')
+    refs = {}
+    for n, n2_list in nodes_ady.items():
+        v1 = get_create_node(refs, g, n)
+        for n2 in n2_list:
+            v2 = get_create_node(refs, g, n2)
+            e = g.add_edge(v1, v2)
+            if n2.maint is None:
+                g.ep.maint[e] = ""
+            else:
+                g.ep.maint[e] = n2.maint
+
+    return g, refs
 
 
 if __name__ == '__main__':
     import package.params as params
     import data.template_data as td
+
     path = params.PATHS['data'] + 'template/201903120540/template_in.xlsx'
     data_in = td.import_input_template(path)
 
     import data.test_data as test_d
+
+
     def temp():
         from importlib import reload
         reload(test_d)
+
+
     data_in = test_d.dataset1()
 
     instance = inst.Instance(data_in)
     res = instance.get_resources().keys_l()[0]
-    node = resource_to_node(instance, res)
-    final_paths = walk_over_nodes(node)
-    all_nodes = walk_over_nodes(node, get_nodes_only=True)
-    len(all_nodes)
+    source = get_source_node(instance, res)
+    sink = get_sink_node(instance, res)
+    # final_paths = walk_over_nodes(source)
+    nodes_ady = walk_over_nodes(source, get_nodes_only=True)
+    g, refs = adjacency_to_graph(nodes_ady)
+    g_source = refs[source]
+    g_sink = refs[sink]
+
+    import graph_tool.all as gr
+    import numpy.random as rn
+
+    y_ranges = dict(M=lambda: 0,
+                    VG=lambda: rn.uniform(10, 15) * (1 - 2 * (rn.random() > 0.5)),
+                    VI=lambda: rn.uniform(5, 10) * (1 - 2 * (rn.random() > 0.5)),
+                    VS=lambda: rn.uniform(0, 5) * (1 - 2 * (rn.random() > 0.5))
+                    )
+    # colors = dict(VG='green')
+    colors = \
+        {'VG': '#4cb33d',
+         'VI': '#00c8c3',
+         'VS': '#31c9ff',
+         'M': '#878787',
+         'VG+VI': '#EFCC00',
+         'VG+VS': '#EFCC00',
+         'VG+VI+VS': '#EFCC00',
+         'VI+VS': '#EFCC00'}
+
+    pos = g.new_vp('vector<float>')
+    size = g.new_vp('double')
+    shape = g.new_vp('string')
+    color = g.new_vp('string')
+    first = instance.get_param('start')
+
+    for v in g.vertices():
+        x = instance.get_dist_periods(first, g.vp.period[v])
+        maint = g.vp.maint[v]
+        y = y_ranges.get(maint, lambda: 0)()
+        # if g.vp.maint[v] == 'M':
+        #     y = 0
+        pos[v] = (x, y)
+        size[v] = 2
+        shape[v] = 'circle'
+        color[v] = colors.get(maint, 'red')
+
+    gr.graph_draw(g, pos=pos, vertex_text=g.vp.period,
+                  edge_text=g.ep.maint, vertex_shape=shape, vertex_fill_color=color)
+
+    final_paths = [p for p in gr.all_paths(g, source=g_source, target=g_sink)]
+    len(final_paths)
+    len(nodes_ady)
     # print(len(graph))
     # print(len(end_nodes))
-    print(len(final_paths))
+    # print(len(final_paths))
     # unique_data = [list(x) for x in set(tuple(x) for x in final_paths)]
     # print(len(unique_data))
-    print(final_paths[0])
-    print(final_paths[-1])
+    # print(final_paths[0])
+    # print(final_paths[-1])
     # print(final_paths)
