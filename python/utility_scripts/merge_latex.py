4121b3ea82dd844d26f675de1bfb8954627b0c60
pchtsp <franco.peschiera@gmail.com>
2020-01-31 19:40:36 +0100

    edited merge_latex. What a great tool.

--- python/utility_scripts/merge_latex.py
+++ python/utility_scripts/merge_latex.py
@@ -1,9 +1,20 @@
 import utility_scripts.flatex as fl
 import os
+import pytups as pt
 
-base_file = r'C:\Users\pchtsp\Documents\projects\NPS2019\article.tex'
-output_file = r'C:\Users\pchtsp\Documents\projects\NPS2019\manuscript.tex'
-current_path = os.path.split(base_file)[0]
-g = open(output_file, "w")
-g.write(''.join(fl.expand_file(base_file, current_path, False, False)))
-g.close()
+directory = r'/home/pchtsp/Documents/projects/NPS2019/'
+main_file = 'article.tex'
+manuscript_file = 'manuscript.tex'
+base_file = os.path.join(directory, main_file)
+output_file = os.path.join(directory, manuscript_file)
+
+def edit_file(base_file, output_file):
+    current_path = os.path.split(base_file)[0]
+    g = open(output_file, "w")
+    g.write(''.join(fl.expand_file(base_file, current_path, True, False)))
+    g.close()
+
+
+# merge .tex
+edit_file(base_file, output_file)
+
